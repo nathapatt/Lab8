43850acae88b0b9ba49f53b5cc34f7888a8a1e07
Mind <nathapat.mind@gmail.com>
2022-12-20 14:55:16 +0700

    Lab8_1

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    int a  =  20; b = 30;
-    int c = 4, output;
+    float a  =  20, b = 30;
+    float c = 4, outpot;
 
-    outpot = a - b/c;
-    count << output: << output << \n;
+    outpot = a - (b/c);
+    cout << "output: " << outpot << "\n";
     return 0;
 }
